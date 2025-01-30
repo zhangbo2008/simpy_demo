0cc7eb29c77d166d170bca60ed7b53d497fbb5be
张博 <15122306087@163.com>
2025-01-31 00:03:15 +0800

    no_comment

--- mydemo/3.py
+++ mydemo/3.py
@@ -16,7 +16,23 @@ class School:
     def bell(self):
         for i in range(2):
             yield self.env.timeout(45)
-            self.class_ends.succeed()
+            self.class_ends.succeed() # 就是让这个事件成功继续运行. 源码
+            '''
+    def succeed(self, value: Optional[Any] = None) -> Event:
+        """Set the event's value, mark it as successful and schedule it for
+        processing by the environment. Returns the event instance.
+
+        Raises :exc:`RuntimeError` if this event has already been triggered.
+
+        """
+        if self._value is not PENDING:
+            raise RuntimeError(f'{self} has already been triggered')
+
+        self._ok = True
+        self._value = value
+        self.env.schedule(self)
+        return self    也就是设置ok, 和这次event的返回值, 然后调用schedule让env执行后续代码.
+            '''
             # print('继续让学生上课')
             self.class_ends = self.env.event()
             print('--------',env.now)
@@ -24,7 +40,7 @@ class School:
     def pupil(self):
         for i in range(2):
             print(r' \o/',env.now, end='')
-            yield self.class_ends
+            yield self.class_ends #==这里就是等self.class_ends这个事件结束, 再进行继续print.
 import simpy
 env = simpy.Environment()
 school = School(env)
